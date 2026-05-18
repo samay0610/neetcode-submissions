c45b66322001852fa7e1a0d2552191cd6bb6c073
samay0610 <106040571+samay0610@users.noreply.github.com>
2026-05-18 15:01:21 -0400

    Add: python-class-vs-instance-attributes - submission-0

---
+++ Python OOP/python-class-vs-instance-attributes/submission-0.py
@@ -0,0 +1,24 @@
+class BankAccount: 
+    # TODO: Add class and instance attributes at their appropriate places
+    total_accounts = 0
+    total_balance = 0
+    
+    def __init__(self, name : str, balance : int) -> None:
+        self.name = name
+        self.balance = balance
+        BankAccount.total_accounts += 1
+        BankAccount.total_balance += balance
+
+alice = BankAccount("Alice", 1000)
+bob = BankAccount("Bob", 2000)
+
+print(f"{alice.name}'s balance: ${alice.balance}")
+print(f"{bob.name}'s balance: ${bob.balance}")
+print(f"Total Accounts: {BankAccount.total_accounts}")
+print(f"Total Balance: ${BankAccount.total_balance}")
+
+
+
+# TODO: Create two accounts
+# TODO: Print the information using the mentioned format
+
