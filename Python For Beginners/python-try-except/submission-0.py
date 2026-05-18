9145d824127d11042311f7595d5960822b369162
samay0610 <106040571+samay0610@users.noreply.github.com>
2026-05-18 00:57:07 -0400

    Add: python-try-except - submission-0

---
+++ Python For Beginners/python-try-except/submission-0.py
@@ -0,0 +1,14 @@
+def divide_numbers(a: int, b: int) -> None:
+    try:
+        x = a/b
+        print(x)
+    except:
+        print("An error occurred!")
+    
+
+
+
+# do not modify below this line
+divide_numbers(10, 2)
+divide_numbers(12, 3)
+divide_numbers(2, 0)
